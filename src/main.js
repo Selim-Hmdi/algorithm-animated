c2f136c55e21ff72fdc3c4867ce6814c26c11252
Selim Hoummadi <selim_hoummadi@hotmail.com>
2023-08-12 20:40:43 +0200

    Add vue-router library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import * as VueRouter from "vue-router";
+import SortingAlgorithmsPage from "./components/view/SortingAlgorithmsPage.vue";
+import AboutPage from "./components/view/AboutPage.vue";
+import HomePage from "./components/view/HomePage.vue";
 import "./assets/global.css";
 
-createApp(App).mount("#app"); 
+const routes = [
+    {path: "/", HomePage},
+    {path: "/sorting-algorithms", SortingAlgorithmsPage},
+    {path: "/about", AboutPage}
+];
+
+const router = new VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+});
+
+let app = createApp(App);
+app.use(router);
+
+app.mount("#app"); 
+
